f4f2e92df8c9081386127da23a238af83edca46f
Cameron Partee <cp3082@nyu.edu>
2019-04-23 17:22:03 -0400

    Add files via upload

---
+++ ShootingScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingScript : MonoBehaviour {
+
+    public GameObject explosion;
+    public GameObject gameCamera;
+
+
+
+    void Start() {
+        
+    }
+
+
+    void Update() {
+        
+    }
+
+    public void Shoot() {
+
+        RaycastHit hit;
+        if(Physics.Raycast(gameCamera.transform.position, gameCamera.transform.forward, out hit)) {
+            if(hit.transform.name == "Bottle") {
+                Destroy(hit.transform.gameObject);
+                GameObject go = Instantiate(explosion, hit.point, Quaternion.LookRotation(hit.normal));
+            }
+        }
+    }
+
+
+
+}
